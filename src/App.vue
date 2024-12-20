c2051168cc3f02a7a918b44a870e3ebccaff86c3
PatrikF1 <patrik.fabijanic9@gmail.com>
2024-12-20 22:40:26 +0100

    uredivanje pocetne stranice dovrseno

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
-  <div id="app" class="flex flex-col min-h-screen">
-    <h1>SharpApp</h1>
-    <h1 class="text-red-500">Testiraj Tailwind</h1>
+  <div id="app" class="app-container">
+  
+    <img src="/images/logo.png" alt="logo" class="logo">
 
-    <button class="bg-[#27374D]">
-      <router-link to="/login" class="text-white text-lg">Prijava</router-link>
-    </button>
-    <button class="bg-[#27374D]">
-      <router-link to="/registration" class="text-white text-lg">Registracija</router-link>
-    </button>
+    <div class="button-container">
+      <button class="gumblog">
+        <router-link to="/login" class="link-text">Prijava</router-link>
+      </button>
+      <button class="gumbreg">
+        <router-link to="/registration" class="link-text">Registracija</router-link>
+      </button>
+    </div>
+
+    <img src="/images/pocetna.png" alt="pocetna" class="bottom-image">
   </div>
 </template>
 
 <script setup></script>
 
-<style></style>
+<style>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  background-color: #DDE6ED;
+  position: relative;
+}
+
+.logo {
+  width: 200px;
+  height: auto;
+  margin-top: 3.5rem; 
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4.5rem;
+  flex-grow: 0;
+}
+
+.gumblog, .gumbreg {
+  background-color: #27374D;
+  padding: 0.6rem 1.0rem; 
+  border-radius: 0.5rem; 
+  color: white;
+  font-size: 20px; 
+  font-weight: bold;
+  width: 10rem; 
+  transition: background-color 0.3s;
+}
+
+.gumblog:hover, .gumbreg:hover {
+  background-color: #1D2B3A;
+}
+
+.gumblog { 
+  margin-right: 2rem;
+}
+
+.gumbreg {
+  margin-top: 1rem; 
+  margin-right: -2rem;
+}
+
+.link-text {
+  color: white;
+  text-decoration: none;
+}
+
+.bottom-image {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-bottom: 0;
+}
+</style>
