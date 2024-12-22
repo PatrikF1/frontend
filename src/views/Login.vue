ca7c23d238f1ceeced61a17f1abf5875e8c9d292
PatrikF1 <patrik.fabijanic9@gmail.com>
2024-12-22 15:49:42 +0100

    uspijesno spajanje s backendom i koristenje axiosa, meni osobno laksi nacin nego onaj s biljeski sa nastave

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,29 @@
 <template>
  
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[#DDE6ED]">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-40 w-50" src="/images/logo.png" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+      <h2 class="mt-10 text-center text-2xl/9 tracking-tight text-[#27374D] ">Sign in to your account</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form @submit.prevent="prijava" class="space-y-6" action="#" method="POST">
         <div>
-          <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <label for="email" class="block text-sm/6 font-medium text-[#27374D]">Email address</label>
           <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input v-model="email" type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
           </div>
         </div>
 
         <div>
           <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+            <label for="password" class="block text-sm/6 font-medium text-[#27374D]">Password</label>
             <div class="text-sm">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+              <a href="#" class="font-semibold text-[#27374D] ">Forgot password?</a>
             </div>
           </div>
           <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input v-model="password" type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
           </div>
         </div>
 
@@ -35,7 +35,7 @@
       <p class="mt-10 text-center text-sm/6 text-gray-500">
         Niste Registrirani?
         {{ ' ' }}
-        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Registrirajte se</a>
+        <a href="#" class="font-semibold text-[#27374D] ">Registrirajte se</a>
       </p>
     </div>
   </div>
@@ -43,6 +43,31 @@
 
 
 <script setup>
+
+import { ref } from "vue";
+import axios from "axios"
+
+const email = ref("")
+const password = ref("")
+const poruka = ref("") 
+
+
+async function prijava() {
+  poruka.value = ""
+  try {
+    const response = await axios.post("http://localhost:3000/api/login", {
+      email: email.value,
+      password: password.value
+    })
+    poruka.value = "Korisnik je uspijesno ucitan"
+    console.log("korisnik prijavljen", response.data)
+  } catch (error) {
+    poruka.value = "Greska u prijavi"
+    console.error(error)
+  }
+}
+
+
 
 </script>
 
