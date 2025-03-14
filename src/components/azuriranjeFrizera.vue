d28e649f8c18409540059e5d9810432c5e23baaa
Patrik Fabijanić <patrik.fabijanic9@gmail.com>
2025-03-14 18:06:53 +0100

    pokusavam napraviti azuriranje al se bunim :(

--- src/components/azuriranjeFrizera.vue
+++ src/components/azuriranjeFrizera.vue
@@ -1,20 +1,21 @@
 <template>
 
-<div>
-    Brisanje frizera: 
-    <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" v-for="frizer in frizeri" @click="izbrisiFrizera(frizer._id)">{{ frizer.ime }} {{ frizer.prezime }} - {{ frizer.iskustvo }} </button>
-</div>
+    <div>
+        Azuriranje frizera: 
+        <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" v-for="frizer in frizeri">{{ frizer.ime }} {{ frizer.prezime }} - {{ frizer.iskustvo }} </button>
+    </div>
+    
+    </template>
 
-</template>
-
-<script setup>
-
+    <script setup>
 import backend from '@/backend';
 import {onMounted, ref} from 'vue'
 
 const frizeri = ref([])
 const poruka = ref('')
 const loading = ref(true)
+const novoIskustvo = ref('')
+const id = ref('')
 
 async function dohvatiFrizera () {
   poruka.value = ''
@@ -36,21 +37,22 @@ async function dohvatiFrizera () {
   }
 }
 
-
-async function izbrisiFrizera(id) {
+async function azurirajFrizera(id) {
     try {
-        const response = await backend.delete(`/frizeri/${id}`)
         frizeri.value = frizeri.value.filter(frizer => frizer._id !== id)
-        window.location.reload()
-        console.log(response.data)
+        const f = frizeri.value
+        const respone = await backend.patch(`/frizeri/${f}`, {
+            iskustvo: novoIskustvo.value
+        })
+
+        await dohvatiFrizera()
+
     } catch (error) {
-        console.error(error.message)
+        
     }
 }
 
 onMounted(() => {
     dohvatiFrizera()
 })
-
-
 </script>
--- src/views/adminPage.vue
+++ src/views/adminPage.vue
@@ -1,11 +1,13 @@
 <script setup >
 import dodavanjeFrizera from '@/components/dodavanjeFrizera.vue';
 import brisanjeFrizera from '@/components/brisanjeFrizera.vue'
+import azuriranjeFrizera from '@/components/azuriranjeFrizera.vue';
 </script>
 
 <template>
   <dodavanjeFrizera />
   <brisanjeFrizera />
+  <azuriranjeFrizera />
   <div>
 
    
