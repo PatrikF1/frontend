fc7a35239b7b9227cc58449c4a333d7ffb85c48e
Patrik Fabijanić <patrik.fabijanic9@gmail.com>
2025-03-14 16:23:36 +0100

    okej sada brisemo frizera dodao sam sve iz dohvacanja sad moram skuzit brisanje

--- src/components/brisanjeFrizera.vue
+++ src/components/brisanjeFrizera.vue
@@ -1,11 +1,42 @@
 <template>
 
 <div>
-    Brisanje frizera
+    Brisanje frizera: 
+    <button v-for="frizer in frizeri" @click="izbrisiFrizera">{{ frizer.ime }} {{ frizer.prezime }} - {{ frizer.iskustvo }} </button>
 </div>
 
 </template>
 
 <script setup>
 
+import backend from '@/backend';
+import {onMounted, ref} from 'vue'
+
+const frizeri = ref([])
+const poruka = ref('')
+const loading = ref(true)
+
+async function dohvatiFrizera () {
+  poruka.value = ''
+  try {
+    const response = await backend.get('/frizeri')
+    console.log(response.data)
+    frizeri.value = response.data
+  } catch (error) {
+    console.error(error.message)
+    if (error.response && error.response.data) {
+      poruka.value = error.response.data.message || error.response.data
+    } else {
+      poruka.value = 'Doslo je do greske'
+    }
+
+    alert(poruka.value)
+  } finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+    dohvatiFrizera()
+})
 </script>
