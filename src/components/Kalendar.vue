b23c66b4ff9ef18f7e892728758b73259335c38b
Patrik Fabijanić <patrik.fabijanic9@gmail.com>
2025-04-18 23:03:17 +0200

    evo skuzio sam kako ja nisam formatirao datum tjst jesam u date a mongo kreira svoju vremensku zonu i zato mi se nije poklapalo, sad je u obliku stringa i radi ali necu imat buduce i prosle jer neznam kako to napravit morat cu vidit a necu nazad formatirat jer ce mi se sve zbagirat u kodu

--- src/components/Kalendar.vue
+++ src/components/Kalendar.vue
@@ -2,7 +2,7 @@
     
     <VDatePicker class="kalendar" v-model="date" mode="dateTime" :rules="rules" is24hr/>
     <div class="gumbo">
-    <button @click="spremiTermin" type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-grey-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Spremi Termin!</button>
+    <button @click="spremiTermin" type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-blue-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Spremi Termin!</button>
   </div>
   </template>
   
@@ -23,12 +23,22 @@
  let poruka = ref("")
 
  async function spremiTermin() {
-  poruka = "Termin je uspiješno rezerviran!"
-    Store.postaviDatum(date.value)
-    await Store.spremiTermin()
-    router.push({path: '/home'})
-    alert(poruka)
-  }
+  poruka = "Termin je uspiješno rezerviran!";
+
+  const lokalnoVrijeme = date.value.toLocaleString('hr-HR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  Store.postaviDatum(lokalnoVrijeme); 
+  await Store.spremiTermin();
+  router.push({ path: '/home' });
+  alert(poruka);
+}
+
   
  
 
--- src/views/sviTermini.vue
+++ src/views/sviTermini.vue
@@ -1,6 +1,5 @@
 <template>
 <div class="sviTermini">
-<section>
     <div>
       <h1 class="naslov">
         Termini
@@ -9,49 +8,30 @@
     <div>
         <h1 class="centar">Trenutni rezervirani termini: </h1>
         <div class="popisTrenutnih">
-          <div v-if="trenutniTermin.length > 0">
-            <div v-for="termin in trenutniTermin">
+            <div v-for="termin in termini">
               <li>
                 <div>{{ termin.korisnik.email }}</div>
                 <div>{{ termin.frizer.ime }}</div>
                 <div>{{ termin.usluga.usluga }}</div>
-                <div>{{ formatirajDatum(termin.datum) }}</div>
+                <div>{{ termin.datum }}</div>
               </li>
             </div>
-          </div>
-          <div v-else>
-            <p>Nema trenutnih termina!</p>
-          </div>
-        </div>
-        <h1 class="centar">Prošli termini: </h1>
-        <div class="popisProslih">
-          <div v-if="prosliTermin.length > 0">
-            <div v-for="termin in prosliTermin">
-              <li>
-                <div>{{ termin.korisnik.email }}</div>
-                <div>{{ termin.frizer.ime }}</div>
-                <div>{{ termin.usluga.usluga }}</div>
-                <div>{{ formatirajDatum(termin.datum) }}</div>
-              </li>
-            </div>
-          </div>
-          <div v-else>
-            <p>Nema prošlih termina!</p>
-          </div>
         </div>
     </div>
-</section>
+</div>
+
+
 <div class="kuca">
     <svg @click="home" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
   <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 </svg>
 </div>
-</div>
+
 </template>
 
 <script setup>
 import backend from '@/backend';
-import {onMounted, ref, computed} from 'vue'
+import {onMounted, ref} from 'vue'
 import { useRouter } from 'vue-router';
 
 const termini = ref([])
@@ -78,18 +58,6 @@ try {
 }
 }
 
-const trenutniTermin = computed(() => {
-    return termini.value.filter(termin => new Date(termin.datum) > new Date())
-})
-
-const prosliTermin = computed(() => {
-    return termini.value.filter(termin => new Date(termin.datum) <= new Date())
-})
-
-function formatirajDatum(formatDatum) {
-  const datum = new Date(formatDatum);
-  return datum.toLocaleDateString("hr-HR", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" });
-}
 
 
 async function home() {
@@ -115,8 +83,8 @@ onMounted(() => {
 }
 
 .kuca {
-  margin-top: 47rem;
-  position: absolute;
+  margin-top: 1rem;
+  position: relative;
   color: #1D2B3A;
 }
 
@@ -125,6 +93,7 @@ onMounted(() => {
     margin-top: 3rem;
     text-align: center;
     color: #1D2B3A;
+    text-decoration: underline;
     
 }
 
