605d86535823befccdd11a4dbb3055129b1695df
Patrik Fabijanić <patrik.fabijanic9@gmail.com>
2025-04-18 21:02:02 +0200

    malo sam preuredio i nedostatke nadodao moram jos vidit sta s tim datumom da provjerava i to

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -7,10 +7,20 @@
 <script setup>
 import { onMounted } from 'vue'
 import 'leaflet/dist/leaflet.css'
-
 import L from 'leaflet'
 
 
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41]
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
 
 onMounted(() => {
   const map = L.map('map').setView([45.431, 13.524], 13) 
@@ -21,10 +31,8 @@ onMounted(() => {
 
   const marker = L.marker([45.431, 13.524]).addTo(map)
   marker
-    .bindPopup('<b>Ovdje smo')
+    .bindPopup('<b>Trgovačka 12')
     .openPopup()
-
- 
 })
 </script>
 
--- src/views/sviTermini.vue
+++ src/views/sviTermini.vue
@@ -1,17 +1,53 @@
 <template>
-<div class="popisTrenutnih">
-    <h1>Trenutni rezervirani termini: </h1>
-    <li v-for="termin in trenutniTermin"> {{ termin.korisnik.email }} - {{ termin.frizer.ime }} - {{ termin.usluga.usluga }} - {{ formatirajDatum(termin.datum) }} </li>
+<div class="sviTermini">
+<section>
+    <div>
+      <h1 class="naslov">
+        Termini
+      </h1>
+    </div>
+    <div>
+        <h1 class="centar">Trenutni rezervirani termini: </h1>
+        <div class="popisTrenutnih">
+          <div v-if="trenutniTermin.length > 0">
+            <div v-for="termin in trenutniTermin">
+              <li>
+                <div>{{ termin.korisnik.email }}</div>
+                <div>{{ termin.frizer.ime }}</div>
+                <div>{{ termin.usluga.usluga }}</div>
+                <div>{{ formatirajDatum(termin.datum) }}</div>
+              </li>
+            </div>
+          </div>
+          <div v-else>
+            <p>Nema trenutnih termina!</p>
+          </div>
+        </div>
+        <h1 class="centar">Prošli termini: </h1>
+        <div class="popisProslih">
+          <div v-if="prosliTermin.length > 0">
+            <div v-for="termin in prosliTermin">
+              <li>
+                <div>{{ termin.korisnik.email }}</div>
+                <div>{{ termin.frizer.ime }}</div>
+                <div>{{ termin.usluga.usluga }}</div>
+                <div>{{ formatirajDatum(termin.datum) }}</div>
+              </li>
+            </div>
+          </div>
+          <div v-else>
+            <p>Nema prošlih termina!</p>
+          </div>
+        </div>
+    </div>
+</section>
+<div class="kuca">
+    <svg @click="home" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
 </div>
-<div class="popisProslih">
-    <h1>Prošli termini: </h1>
-    <li v-for="termin in prosliTermin"> {{ termin.korisnik }} - {{ termin.frizer.ime }} - {{ termin.usluga.usluga }} - {{ formatirajDatum(termin.datum) }} </li>
 </div>
-
-<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="nazad">Home</button>
-
 </template>
-
 
 <script setup>
 import backend from '@/backend';
@@ -56,8 +92,9 @@ function formatirajDatum(formatDatum) {
 }
 
 
-function nazad() {
-    router.push({path: '/home'})
+async function home() {
+  sessionStorage.clear()
+  router.push({path: "/home"})
 }
 
 onMounted(() => {
@@ -68,4 +105,33 @@ onMounted(() => {
 
 
 <style>
+.sviTermini {
+  background-color: #dde6ed;
+  flex-direction: column;
+  min-height: 100vh;
+  display: flex;
+  overflow: scroll;
+  align-items: center;
+}
+
+.kuca {
+  margin-top: 47rem;
+  position: absolute;
+  color: #1D2B3A;
+}
+
+.centar {
+    font-size: 1.25rem;
+    margin-top: 3rem;
+    text-align: center;
+    color: #1D2B3A;
+    
+}
+
+.popisTrenutnih, .popisProslih{
+    margin-top: 1rem;
+    list-style-type: none;
+    text-align: center;
+    color: #1D2B3A;
+}
 </style>
