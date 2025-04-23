9758edff45f0fbc2ca0897790b7c6fd0887dca50
Patrik Fabijanić <patrik.fabijanic9@gmail.com>
2025-04-23 11:49:00 +0200

    sad sam dodao u backend da se spoji na javni render url

--- src/backend.js
+++ src/backend.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 const baseURL = process.env.VUE_APP_BASE_URL;
 
+const mainURL = process.env.VUE_APP_API_URL;
+
 const backend = axios.create({
+  mainURL,
   baseURL, 
   timeout: 2000, 
 });
 
 export default backend;
+
